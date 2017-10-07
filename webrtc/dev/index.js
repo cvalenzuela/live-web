f9e449743ad57c09e851412246ab647c940966c2
Cristobal Valenzuela <cvalenzuela@nyu.edu>
2017-10-07 13:27:30 -0400

    server with es6

---
+++ webrtc/dev/index.js
@@ -0,0 +1,40 @@
+/*
+Server
+*/
+
+import * as fs from 'fs';
+// const credentials = {
+//   key: fs.readFileSync('./private/my-key.pem'),
+//   cert: fs.readFileSync('./private/my-cert.pem')
+// };
+let express = require('express');
+const app = express();
+let server;
+
+if(process.env.PROD){
+  console.log('Starting production server with https');
+  server = require('https').Server(credentials, app);
+} else {
+  console.log('Development server without https');
+  server = require('http').Server(app);
+}
+
+const io = require('socket.io')(server);
+const path = require('path');
+const PORT = process.env.PORT || 8765;
+
+app.use(express.static(__dirname + '/public/'));
+
+app.get('/', function (req, res) {
+  res.sendFile(__dirname + '/public/index.html');
+});
+
+io.on('connection', (socket) => {
+  handleSocket(io, socket);
+});
+
+server.listen(PORT, () => {
+  let host = server.address().address;
+  let port = server.address().port;
+  console.log('running at http://' + host + ':' + port);
+});
---
+++ webrtc/dev/public/js/test.js
@@ -0,0 +1,3 @@
+let test = 'aaaaa mundo';
+
+export { test };
